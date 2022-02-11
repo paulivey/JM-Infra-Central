b5da4e919dbf5df8c5d6da0b5799287defe8da17
Paul Ivey <paivey@microsoft.com>
2022-02-11 14:21:51 +0000

    Added sandbox.tfvars file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,23 @@
 variable "app_name" {
     type    = string
-    default = "bluehydrogenabacus"
 }
 
 variable "location" {
     type    = string
-    default = "northeurope"
 }
 
 variable "environment"  {
     type    = string
-    default = "SBX"
 }
 
 variable "project"  {
     type    = string
-    default = "DSOPS"
 }
 
 variable "region_short"  {
     type    = string
-    default = "EU"
 }
 
 variable "app_suffix"  {
     type    = string
-    default = "001"
 }
