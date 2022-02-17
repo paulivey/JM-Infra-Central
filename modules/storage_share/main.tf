e431ff21d00d7313befb2083a7ac4601bc8f2d68
Paul Ivey <47188956+paul-msft@users.noreply.github.com>
2022-02-17 19:21:32 +0000

    Update main.tf

--- modules/storage_share/main.tf
+++ modules/storage_share/main.tf
@@ -1,6 +1,6 @@
 # Create storage share
 resource "azurerm_storage_share" "share" {
-  name                 = lower(var.share_name)
+  name                 = var.share_name
   storage_account_name = var.sta_name
   quota                = var.share_quota
 }
