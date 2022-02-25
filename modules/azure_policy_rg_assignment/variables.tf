87bc3e26221d4fcdf51c8eb8dbae159858344176
Paul Ivey <paivey@microsoft.com>
2022-02-25 22:00:48 +0000

    Amended policy definition

--- modules/azure_policy_rg_assignment/variables.tf
+++ modules/azure_policy_rg_assignment/variables.tf
@@ -4,7 +4,7 @@ variable "resource_group_name" {
 
 variable "policy_definition_id" {
     type    = string
-    default = "/subscriptions/8d49d625-7846-4382-a9ba-7470576eb940/providers/Microsoft.Authorization/policyDefinitions/81be1eda-581f-481f-afda-4237b6dcb0a0"
+    default = "/subscriptions/8d49d625-7846-4382-a9ba-7470576eb940/providers/Microsoft.Authorization/policyDefinitions/5ce0e437-c7df-421d-b19f-77d7988770ea"
 }
 
 variable "tag_country" {
