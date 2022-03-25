599d970379a39122343b2bec3cae260481252a4c
Paul Ivey <paivey@microsoft.com>
2022-03-25 16:10:20 +0000

    Fixed reference to wep_app

--- modules/web_app_linux/main.tf
+++ modules/web_app_linux/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 # Create app service web app
-resource "azurerm_linux_web_app" "name" {
+resource "azurerm_linux_web_app" "web_app" {
   # Example - Only deploy this if var.asp_kind = Linux
   count = var.asp_kind == "Linux" ? 1 : 0
   name = var.app_name
@@ -32,22 +32,3 @@ resource "azurerm_linux_web_app" "name" {
     }
   }
 }
-# resource "azurerm_app_service" "web_app" {
-#     name                = var.app_name
-#     resource_group_name = var.app_rg_name
-#     location            = var.location
-#     app_service_plan_id = azurerm_service_plan.asp.id
-#     client_cert_mode    = var.app_cert_mode
-#     app_settings        = var.app_settings
-#     tags                = var.tags
-    
-#     site_config {
-#       number_of_workers        = var.app_site_cfg_num_workers
-#       linux_fx_version         = var.app_linux_fx_version
-#       dotnet_framework_version = var.app_dotnet_version
-#       remote_debugging_version = var.app_rmt_debug_version
-#       app_command_line         = var.app_cmd_line
-#       ftps_state               = var.app_ftps_state
-#       vnet_route_all_enabled   = var.route_all_enabled
-#     }
-# }
