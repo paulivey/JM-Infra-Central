ddcefd262f0b805142af6d6807114cc8a18d8bd1
Paul Ivey <paivey@microsoft.com>
2022-03-25 17:04:15 +0000

    Added conditional RBAC

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Make the developer an owner of the resource group
 resource "azurerm_role_assignment" "role_assignment"{
+  count = var.owner_object_id == "" ? 1 : 0
   scope = azurerm_resource_group.rg.id
   role_definition_name = "Contributor"
   principal_id = var.owner_object_id
