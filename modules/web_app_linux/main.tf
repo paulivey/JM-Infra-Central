ac2747d11bdf70198873b9c42ec4ebb9603fb027
Paul Ivey <paivey@microsoft.com>
2022-03-25 13:15:45 +0000

    Updated web_app_linux module removing the deprecated ASP resource

--- modules/web_app_linux/main.tf
+++ modules/web_app_linux/main.tf
@@ -1,18 +1,13 @@
 # Create app service plan
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
     name                         = var.asp_name
     location                     = var.location
     resource_group_name          = var.app_rg_name
-    kind                         = var.asp_kind
+    os_type                      = var.asp_kind
     reserved                     = var.asp_reserved
     maximum_elastic_worker_count = var.asp_max_ew_count
     tags                         = var.tags
-
-    sku {
-      capacity = var.asp_capacity
-      size     = var.asp_size
-      tier     = var.asp_tier
-    }
+    sku_name                     = var.size
 }
 
 # Create app service web app
@@ -20,7 +15,7 @@ resource "azurerm_app_service" "web_app" {
     name                = var.app_name
     resource_group_name = var.app_rg_name
     location            = var.location
-    app_service_plan_id = azurerm_app_service_plan.asp.id
+    app_service_plan_id = azurerm_service_plan.asp.id
     client_cert_mode    = var.app_cert_mode
     app_settings        = var.app_settings
     tags                = var.tags
